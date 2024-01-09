f128bb0275904ce6d1cfaee8e97a3bd175d70188
Ghazi Hicheri <ghazi123@hotmail.se>
2024-01-09 13:26:58 +0100

    first commit

---
+++ 13-rock-paper-scissors.js
@@ -0,0 +1,101 @@
+let score = JSON.parse(localStorage.getItem('score'));
+
+            if(!score){
+                score = {
+                    wins:0,
+                    losses:0,
+                    ties:0
+                };
+            }
+
+            updateScoreElement();
+
+           function playGame(playerMove){
+                let result = '';
+                const computerMove = pickComputerMove();
+                if(playerMove === 'Scissors')
+                {
+                    if(computerMove === 'Rock'){
+                        result = 'You lose.';
+                    } else if(computerMove === 'Paper'){
+                        result = 'You win.'
+                    }else if(computerMove === 'Scissors'){
+                        result = 'Tie.'
+                    }
+                }
+                else if(playerMove === 'Paper'){
+                    if(computerMove === 'Rock'){
+                        result = 'You win.';
+                    } else if(computerMove === 'Paper'){
+                        result = 'Tie.'
+                    }else if(computerMove === 'Scissors'){
+                        result = 'You lose.'
+                    }
+                }
+                else if(playerMove === 'Rock')
+                {
+                    if(computerMove === 'Rock'){
+                        result = 'Tie.';
+                    } else if(computerMove === 'Paper'){
+                        result = 'You lose.'
+                    }else if(computerMove === 'Scissors'){
+                        result = 'You win.'
+                    }
+                }
+                
+                if(result === 'You win.'){
+                    score.wins += 1;
+                } else if (result === 'You lose.'){
+                    score.losses += 1;
+                } else if(result === 'Tie.'){
+                    score.ties += 1;
+                }
+
+                localStorage.setItem('score', JSON.stringify(score));
+
+                updateScoreElement();
+
+                document.querySelector('.js-result').innerHTML = result;
+                document.querySelector('.js-moves').innerHTML = `You
+                    <img src="images/${playerMove}-emoji.png" class="move-icon">
+                    <img src="images/${computerMove}-emoji.png" class="move-icon">
+                    Computer`;
+
+                console.log(computerMove);
+           }
+           
+            function updateScoreElement() {
+                document.querySelector('.js-score').innerHTML = `Wins: ${score.wins} | Losses: ${score.losses} | Ties: ${score.ties}`;
+
+            }
+        
+            function pickComputerMove(){
+                let computerMove = '';
+                const randomNumber = Math.random();
+
+                if(randomNumber >= 0 && randomNumber < 1/3){
+                    computerMove = 'Rock';
+                } else if (randomNumber >= 1 / 3 && 2/3 < 3){
+                    computerMove = 'Paper';
+                } else if (randomNumber >= 2/3 && randomNumber < 1){
+                    computerMove = 'Scissors'
+                }
+
+                return computerMove;
+            }
+
+            let isAutoPlyaing = false;
+            let intervalId;
+            function autoPlay(){
+                if(!isAutoPlyaing){
+                    intervalId = setInterval(() => {
+                        const playerMove = pickComputerMove();
+                        playGame(playerMove);
+                    }, 1000);
+                }else{
+                    clearInterval(intervalId);
+                    isAutoPlyaing = false;
+
+                }
+                isAutoPlyaing = true;
+            }
